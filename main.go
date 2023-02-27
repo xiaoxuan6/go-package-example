308f04026c9e197652d1928e107ad77750f5c0ba
xiaoxuan6 <1527736751@qq.com>
2023-02-27 14:03:17 +0800

    feature: 使用 swagger 生成文档

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// 生成文档：bee run -gendoc=true -downdoc=true
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
