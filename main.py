a837ee2c331480866366805e827ff3c23f954a92
xiaoxuan6 <1527736751@qq.com>
2023-03-02 19:59:14 +0800

    fix: 已添加的第三方包去重

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import sys
 def writer(url, desc):
     filename = os.getcwd() + '/README.md'
     with open(filename, encoding='utf-8', mode='r') as f:
+        oldContent = f.read()
 
-        item = r"||{}|{}|".format(url, desc)
-        content = r"{}{}{}".format(f.read(), item, "<br>\n")
+        if r"{}".format(oldContent).find(url) == -1:
+            item = r"||{}|{}|".format(url, desc)
+            newContent = r"{}{}{}".format(oldContent, item, "<br>\n")
 
-        with open(filename, encoding='utf-8', mode='w') as fin:
-            fin.write(content)
+            with open(filename, encoding='utf-8', mode='w') as fin:
+                fin.write(newContent)
 
 
 if __name__ == '__main__':
