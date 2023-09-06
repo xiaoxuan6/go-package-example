8a500460b2465742ead9c8831204a9bd56d80e66
xiaoxuan6 <1527736751@qq.com>
2023-09-06 14:31:43 +0800

    refactor: modify main

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
+import argparse
 import os
-import sys
 
 import requests
 from lxml import etree
 
 
-def writer(url, desc):
+def writer(url, desc, demoUrl):
     filename = os.getcwd() + '/README.md'
     with open(filename, encoding='utf-8', mode='r') as f:
         oldContent = f.read()
 
         if r"{}".format(oldContent).find(url) == -1:
-            item = r"||{}|{}|".format(url, desc)
-            newContent = r"{}{}{}".format(oldContent, item, "<br>\n")
 
+            if demoUrl is not None:
+                demo = f"[demo]({demoUrl})"
+                item = r"|{}|{}|{}|".format(demo, url, desc)
+            else:
+                item = r"||{}|{}|".format(url, desc)
+
+            newContent = r"{}{}{}".format(oldContent, item, "<br>\n")
             with open(filename, encoding='utf-8', mode='w') as fin:
                 fin.write(newContent)
 
 
-def check_url(param):
-    if str(param).startswith("http"):
-        url = param
-    else:
-        url = "https://" + param
+def check_url(uri):
+    url = uri
+    if not str(uri).startswith("http"):
+        url = "https://" + uri
 
     response = requests.get(url=url).text.encode('utf-8')
     tree = etree.HTML(response)
     title = tree.xpath('//*[@id="responsive-meta-container"]/div/p/text()')
 
-    url = str(param).replace('https://', '').replace('http://', '')
-    desc = str(title[0]).strip()
-
-    return url, desc
+    return str(title[0]).strip()
 
 
 if __name__ == '__main__':
-    url = ''
-    desc = ''
-    if len(sys.argv) == 3:
-        if len(sys.argv[2]) > 0:
-            url = str(sys.argv[1]).replace('https://', '').replace('http://', '')
-            desc = sys.argv[2]
-        else:
-            url, desc = check_url(sys.argv[1])
-    elif len(sys.argv) == 2:
-        url, desc = check_url(sys.argv[1])
-    else:
-        sys.exit(0)
 
-    writer(url, desc)
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--url', type=str)
+    parse.add_argument('--description', type=str, default=None)
+    parse.add_argument('--demo_url', type=str, default=None)
+    args = parse.parse_args()
+
+    url = args.url
+    desc = args.description
+    if desc is None:
+        desc = check_url(url)
+
+    url = str(url).replace('https://', '').replace('http://', '')
+
+    writer(url, desc, args.demo_url)
