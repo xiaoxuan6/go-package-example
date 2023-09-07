3910522940620237b43ddd9f3a47ca23e631ee23
xiaoxuan6 <1527736751@qq.com>
2023-09-07 16:51:31 +0800

    feat: Add image ascii

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/astaxie/beego v1.12.2
 require (
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
 	github.com/smartystreets/goconvey v1.6.4
+	github.com/zyxar/image2ascii v0.0.0-20230512134409-d14b3a6d3592
 )
--- main.go
+++ main.go
@@ -1,18 +1,60 @@
 package main
 
 import (
-	"package-example/controllers"
-	_ "package-example/routers"
-
-	"github.com/astaxie/beego"
+	"fmt"
+	"github.com/zyxar/image2ascii/ascii"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
 )
 
 func main() {
-	// 生成文档：bee run -gendoc=true -downdoc=true
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	//f, err := os.Open("88888888.jpg")
+	//defer f.Close()
+	//if err != nil {
+	//	_, _ = fmt.Fprintln(os.Stderr, err)
+	//	return
+	//}
+	//
+	//img, _, err := image.Decode(f)
+	//if err != nil {
+	//	_, _ = fmt.Fprintln(os.Stderr, err)
+	//	return
+	//}
+	//bounds := img.Bounds()
+	//width := bounds.Max.X / 4
+	//height := bounds.Max.Y / 4
+	//
+	//fmt.Println("图片宽", width)
+	//fmt.Println("图片高", height)
+
+	opt := ascii.Options{
+		Width:  50,
+		Height: 50,
+		Color:  true,
+		Invert: false,
+		Flipx:  false,
+		Flipy:  false,
 	}
-	beego.ErrorController(&controllers.ErrorController{})
-	beego.Run()
+
+	f1, err := os.Open("88888888.jpg")
+	defer f1.Close()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	a, err := ascii.Decode(f1, opt)
+	if err != nil {
+		fmt.Println("111")
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	_, err = a.WriteTo(os.Stdout)
+	if err != nil {
+		fmt.Println("222")
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		return
+	}
 }
