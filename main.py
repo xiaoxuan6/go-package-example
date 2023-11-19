5556aba36f1fab281dfcc1f3b494bf184b9dfa2d
xiaoxuan6 <1527736751@qq.com>
2023-11-19 13:29:20 +0800

    feat: Add params language

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
 import argparse
 import os
-from urllib.parse import urlparse
+from lxml import etree
 
 import requests
 
 
-def parse_url(uri):
-    parsed_url = urlparse(uri)
-    path = parsed_url.path.strip("/")
-    return path.split("/")
-
-
-def get_filename(owner, repo):
-    uri = 'https://api.github.com/repos/{}/{}/languages'.format(owner, repo)
-    response = requests.get(uri).json()
-    print('response', response)
-
-    if 'Go' in response:
+def get_filename(language):
+    if 'Go' == language:
         return os.getcwd() + '/README.md'
-    elif 'PHP' in response:
+    elif 'PHP' == language:
         return os.getcwd() + '/README_PHP.md'
     else:
         return os.getcwd() + '/README_OTHER.md'
 
 
-def writer(uri, description, demoUrl):
-    paths = parse_url(uri)
-    filename = get_filename(paths[0], paths[1])
-
+def writer(uri, description, language, demoUrl):
+    filename = get_filename(language)
     uri = f'{uri}'.replace('https://', '')
     with open(filename, encoding='utf-8', mode='r') as f:
         oldContent = f.read()
 
         if r"{}".format(oldContent).find(uri) == -1:
 
-            if f'{filename}'.__contains__('README.md'):
+            if language == 'Go':
                 if len(demoUrl) > 1:
                     demo = f"[demo]({demoUrl})"
                     item = r"|{}|{}|{}|".format(demo, uri, description)
@@ -49,17 +37,21 @@ def writer(uri, description, demoUrl):
 
 
 def get_description(uri):
-    paths = parse_url(uri)
-    uri = 'https://api.github.com/repos/{}/{}'.format(paths[0], paths[1])
-    response = requests.get(uri)
-    if response.status_code == 200:
-        return response.json()['description']
+    if not str(uri).startswith("http"):
+        uri = "https://" + uri
+
+    response = requests.get(url=uri).text.encode('utf-8')
+    tree = etree.HTML(response)
+    title = tree.xpath('//*[@id="responsive-meta-container"]/div/p/text()')
+
+    return str(title[0]).strip()
 
 
 if __name__ == '__main__':
     parse = argparse.ArgumentParser()
     parse.add_argument('--url', type=str)
     parse.add_argument('--description', type=str, default='')
+    parse.add_argument('--language', type=str, default='Go')
     parse.add_argument('--demo_url', type=str, default='')
     args = parse.parse_args()
 
@@ -68,4 +60,4 @@ if __name__ == '__main__':
     if len(desc) < 1:
         desc = get_description(url)
 
-    writer(url, desc, args.demo_url)
+    writer(url, desc, args.language, args.demo_url)
