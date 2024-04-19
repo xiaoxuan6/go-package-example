b9d798ff25c0dcdd73cec78b0cd94665cda7fd6d
xiaoxuan6 <1527736751@qq.com>
2024-04-19 20:29:47 +0800

    fix: Update watermark.png

--- watermark.go
+++ watermark.go
@@ -4,11 +4,29 @@ import (
 	"github.com/noelyahan/impexp"
 	"github.com/noelyahan/mergi"
 	"image"
+	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	i, err := mergi.Import(impexp.NewFileImporter("1712295471.png"))
+	dir, _ := os.Getwd()
+	path := filepath.Join(dir, "/../../hexo/mkdocs/docs/static/images/")
+
+	_ = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+
+		if d.IsDir() == true {
+			return nil
+		}
+
+		watermark(path)
+		return nil
+	})
+}
+
+func watermark(img string) {
+	i, err := mergi.Import(impexp.NewFileImporter(img))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,5 +41,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_ = mergi.Export(impexp.NewFileExporter(res, "watermark_1.png"))
+	_ = mergi.Export(impexp.NewFileExporter(res, img))
 }
